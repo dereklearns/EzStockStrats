28ac6f1df4d4011fb9f2a27de230242ebc318df0
dereklearns <derekadam1988@gmail.com>
2022-08-07 20:04:30 -0500

    HTML Inputs for SMA/EMA and refactor data transformation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from dash import Dash, html, dcc, dash_table
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 from dash import Dash, dcc, html, Input, Output
@@ -10,11 +11,12 @@ import numpy as np
 
 app = Dash(__name__)
 
+app = Dash(external_stylesheets = [dbc.themes.BOOTSTRAP])
 # df = merge_buy_and_sell_dates(get_stock_df('GE'))
 df = merge_buy_and_sell_dates(get_stock_df('GE')).reset_index()
 
 colors = {
-    'background': '#111111',
+    'background': '#ffffff',
     'text': '#7FDBFF'
 }
 
@@ -37,7 +39,14 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         'textAlign': 'center',
         'color': colors['text']
     }),
-    dcc.Dropdown(['GE', 'JNJ', 'MSFT'], 'GE', id='input_dropdown'),
+    html.Div(children=[dcc.Dropdown(['GE', 'JNJ', 'MSFT'], 'GE', id='input_dropdown')
+
+    ])
+    ,
+    
+    dcc.Slider(marks={i: str(i) for i in [3, 5, 8, 20, 50, 100]}, value=5, id='input-sma-1'),
+    dcc.RadioItems([5, 8, 12, 20, 50, 100, 200], 100, id='input-ema-1', labelStyle={'display': 'block'}),
+
     html.Div(style={"width": "75%"}, children=[
         
         dcc.Graph(
@@ -49,6 +58,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
             id='table-chart-1',
             columns=[{'name': i, 'id': i} for i in df.columns],
             data=df.to_dict("rows"),
+            style_table={'margin-left': '3vw', 'margin-top': '3vw'},
             style_data={
                 'whiteSpace': 'normal',
                 'height': 'auto',
@@ -58,13 +68,160 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
 
     ])
 
-])
+])  
+
+
+app.layout = html.Div(
+    [
+        dbc.Row(
+                dbc.Col(html.Div(html.H1("EzStockStrats")))),
+
+        dbc.Row(
+            [
+                dbc.Col(html.Div(
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.H4("Underlying", className="card-title"),
+                                html.H6("(SYMBOL/TICKER)", className="card-subtitle"),
+                                html.P(
+                                    "Commonly referred to as a ticker/symbol, the underlying for a traded company.",
+                                    className="card-text",
+                                ),
+                                dcc.Dropdown(['GE', 'JNJ', 'MSFT'], 'GE', id='input_dropdown')
+                                # dcc.RadioItems([5, 8, 12, 20, 50, 100, 200], 100, id='input-ticker-1', labelStyle={'display': 'block'}),
+                            ]
+                        )
+    #                     ,
+    # style={"width": "18rem"},
+),
+
+
+                    
+                    )),
+
+
+
+
+
+
+
+
+
+                dbc.Col(html.Div(
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.H4("Simple Moving Average", className="card-title"),
+                                html.H6("(SMA)", className="card-subtitle"),
+                                html.P(
+                                    "Non-weighted moving average of an underlying."
+                                    "Select the # of days below for the SMA.",
+                                    className="card-text",
+                                ),
+                                dcc.RadioItems([5, 8, 12, 20, 50, 100, 200], 100, id='input-sma-1', labelStyle={'display': 'block'}),
+                            ]
+                        )
+                        # ,
+                        #     # style={"width": "18rem"},
+),
+                    
+
+                )),
+
+                dbc.Col(html.Div(
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.H4("Exponential Moving Average", className="card-title"),
+                                html.H6("(EMA)", className="card-subtitle"),
+                                html.P(
+                                    "Weighted moving average of an underlying."
+                                    "Select the # of days below for the EMA.",
+                                    className="card-text",
+                                ),
+                                dcc.RadioItems([5, 8, 12, 20, 50, 100, 200], 100, id='input-ema-1', labelStyle={'display': 'block'}),
+                            ]
+                        )
+    #                     ,
+    # style={"width": "18rem"},
+),
+
+
+                    
+                    )),
+                
+                dbc.Col(html.Div("One of three columns")),
+            ]
+        ),
+
+
+        dbc.Row([           
+            dbc.Col(
+                
+                html.Div(        
+                    dcc.Graph(
+                    id='line-chart-1'
+        ))),
+
+
+
+
+            dbc.Col(
+                    html.Div(children=[
+        dash_table.DataTable(
+            id='table-chart-1',
+            columns=[{'name': i, 'id': i} for i in df.columns],
+            data=df.to_dict("rows"),
+            style_table={'margin-left': '3vw', 'margin-top': '3vw'},
+            style_data={
+                'whiteSpace': 'normal',
+                'height': 'auto',
+            },
+                style_data_conditional=[
+        {
+            'if': {
+                'filter_query': '{Type} = Buy',
+                # 'column_id': 'Type'
+            },
+            'backgroundColor': '#3D9970',
+            'color': 'white',
+            # 'fontWeight': 'bold'
+        },
+                {
+            'if': {
+                'filter_query': '{Type} = Sell',
+                # 'column_id': 'Type'
+            },
+            'backgroundColor': 'tomato',
+            'color': 'white',
+            # 'fontWeight': 'bold'
+        }
+        
+        ],
+            fill_width=False),
+
+
+    ]))
+
+                    
+                    
+                    
+                    
+    ])
+    ]
+)
+
+
 
 @app.callback(
     Output('line-chart-1', 'figure'),
-    Input('input_dropdown', 'value'))
-def update_figure(selected_stock):
-    filtered_df = get_stock_df(selected_stock)
+    Input('input_dropdown', 'value'),
+    Input('input-sma-1', 'value'),
+    Input('input-ema-1', 'value'))
+def update_figure(selected_stock, sma, ema):
+    filtered_df = get_stock_df(selected_stock, sma, ema)
+    print(sma, ema)
     # print (filtered_df.head(10))
 
 
